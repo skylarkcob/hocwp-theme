bb31fbd2fe16019c8bf784000eb067b6788c6e80
Cuong Dinh Lai <laidinhcuongvn@gmail.com>
2018-06-29 22:13:15 +0700

    Update version 6.4.6.

--- hocwp/load.php
+++ hocwp/load.php
@@ -6,7 +6,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * Theme core version.
  */
-define( 'HOCWP_THEME_CORE_VERSION', '6.4.5' );
+define( 'HOCWP_THEME_CORE_VERSION', '6.4.6' );
 
 /**
  * Theme developing mode.
