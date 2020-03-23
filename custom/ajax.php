f9c955adee3b56f6ace4dd754f48bfb4540eb0dd
Cuong Dinh Lai <laidinhcuongvn@gmail.com>
2020-03-23 11:08:11 +0700

    Add functions for ajax callback

--- custom/ajax.php
+++ custom/ajax.php
@@ -2,3 +2,19 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
+
+/*
+ * Default AJAX callback function.
+ * Use this function name as callback property for data script input.
+ */
+function hocwp_theme_custom_ajax_callback() {
+	HT_Custom()->ajax_callback();
+}
+
+/*
+ * Default AJAX Private callback function.
+ * Use this function name as callback property for data script input.
+ */
+function hocwp_theme_custom_ajax_private_callback() {
+	HT_Custom()->ajax_private_callback();
+}
