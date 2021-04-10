f1ae8ee66bc6a1b5ef90216b971d453e7d090110
Cuong Dinh Lai <laidinhcuongvn@gmail.com>
2021-04-10 08:51:44 +0700

    Update admin-setting-page-system-information.php

--- hocwp/admin/admin-setting-page-system-information.php
+++ hocwp/admin/admin-setting-page-system-information.php
@@ -3,7 +3,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
 
-if ( ! current_user_can( 'manage_options' ) || ! defined( 'HOCWP_THEME_DEVELOPING' ) || ! HOCWP_THEME_DEVELOPING ) {
+if ( ! ( current_user_can( 'manage_options' ) || ( defined( 'HOCWP_THEME_DEVELOPING' ) && HOCWP_THEME_DEVELOPING ) ) ) {
 	return;
 }
 
@@ -11,22 +11,3 @@ $tab = new HOCWP_Theme_Admin_Setting_Tab( 'system_information', __( 'System Info
 
 $tab->callback      = HOCWP_THEME_CORE_PATH . '/admin/views/admin-system-information.php';
 $tab->submit_button = false;
-
-$tab->add_field_array( array(
-	'id'    => 'default_thumbnail',
-	'title' => __( 'Default Thumbnail', 'hocwp-theme' ),
-	'args'  => array(
-		'type'          => 'positive_integer',
-		'callback'      => array( 'HOCWP_Theme_HTML_Field', 'media_upload' ),
-		'callback_args' => array()
-	)
-) );
-
-$args = array(
-	'class' => 'medium-text',
-	'type'  => 'checkbox',
-	'label' => __( 'Allow using custom HTML in term description.', 'hocwp-theme' )
-);
-
-$field = hocwp_theme_create_setting_field( 'term_html_description', __( 'Term HTML Description', 'hocwp-theme' ), '', $args, 'boolean', 'writing' );
-$tab->add_field_array( $field );
